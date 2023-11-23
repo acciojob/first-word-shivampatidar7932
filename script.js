8048af856399b3617c68f1b713ae3fff4647d2a0
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-11-23 11:50:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
-  // your code here
+    if (s === "" || !s.includes(" ")) {
+        return s;
+    } else {
+        return s.split(" ")[0];
+    }
 }
 
 // Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
+const userString = prompt("Enter a string:");
+alert(firstWord(userString));
